eb81a07b3bf9b5d4426ecd5192b725ef6cb4c70a
Gautam Tirkha <gktirkha@gmail.com>
2023-11-16 18:44:20 +0530

    ready to deploy code

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -3,11 +3,5 @@ user = 'gtirkha'
 group = 'gtirkha'
 errorlog = 'error.log'
 accesslog = 'access.log'
-bind = '127.0.0.1:8000'
+bind = 'unix:/run/gunicorn.sock'
 chdir = '/home/gtirkha/Documents/django_learning'
-
-# set Environment variables here
-raw_env = [
-    "SECRET_KEY=*)this_is_my_key(*",
-    "DEBUG=0",
-]
